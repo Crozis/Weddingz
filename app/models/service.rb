4c7b716828bec7a8b88c5efa53c4de67954966ef
Nima Izadi <nim.izadi@gmail.com>
2012-01-20 21:05:19 +0100

    Add overkill method to fetch all service_types

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,4 @@
 class Service < ActiveRecord::Base
-  has_one :service_type
+  belongs_to :service_type
   has_and_belongs_to_many :weddings
 end
--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -1,3 +1,7 @@
 class Wedding < ActiveRecord::Base
   has_and_belongs_to_many :services
+
+  def service_types
+    self.services.map(&:service_type)
+  end
 end
