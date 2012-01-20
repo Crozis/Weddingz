6fb7cb1e0c6e5a6a4e0665443cbf1f5cbb1b3852
Nicolas Dupont <npg.dupont@gmail.com>
2012-01-20 15:29:09 +0100

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weddingz::Application.config.secret_token = '215997ba536db2b96317c9cd64c91a0cc77fd936c76fc9104da4d3f0924cd8b2cb34be8f02856b42448e5173ce7426790d32df7d093ff2f8ec5290e6c5668c97'
