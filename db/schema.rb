e775c2adc06137beadfa5fc6885c7d27b47e3bbf
Nima Izadi <nim.izadi@gmail.com>
2012-02-03 17:01:51 +0100

    Updated DB Schema

--- db/schema.rb
+++ db/schema.rb
@@ -98,7 +98,6 @@ ActiveRecord::Schema.define(:version => 20120203150423) do
     t.integer  "wedding_id"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "weddings", :force => true do |t|
