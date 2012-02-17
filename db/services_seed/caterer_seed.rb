69fa68a319634c7dad94153796b9a6f34c6bd66c
Nima Izadi <nim.izadi@gmail.com>
2012-02-17 11:51:35 +0100

    coma

--- db/services_seed/caterer_seed.rb
+++ db/services_seed/caterer_seed.rb
@@ -10,5 +10,6 @@ Service.create(name: 'Fleuriste Ariston',
                front_picture: 'http://www.aristonflorist.com/art2/ariston_logo.gif',
                pictures_url: 'http://www.aristonflorist.com/art2/shopping_photo.jpg;http://www.aristonflorist.com/art2/wed-corp.jpg;http://www.aristonflorist.com/art2/weekly_photo.jpg;http://www.aristonflorist.com/art2/weddings.jpg', 
                service_type: caterer_type, 
+               summary: 'Cras mattis consectetur purus sit amet fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.', 
                price: 'De 150€ pour des décorations de tables à 1000€ pour la décoration d\'une église')
