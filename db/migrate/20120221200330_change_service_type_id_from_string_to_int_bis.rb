528c692953ea8b80508178c7cb8658e831805af6
Nima Izadi <nim.izadi@gmail.com>
2012-02-21 21:04:14 +0100

    Add a test migration

---
+++ db/migrate/20120221200330_change_service_type_id_from_string_to_int_bis.rb
@@ -0,0 +1,17 @@
+class ChangeServiceTypeIdFromStringToIntBis < ActiveRecord::Migration
+  def change
+    drop_table :services
+    create_table :services do |t|
+      t.string  :name
+      t.string  :phone_number
+      t.string  :address
+      t.text    :front_picture
+      t.string  :pictures_url
+      t.text    :price
+      t.text    :description
+      t.text    :summary
+      t.integer :service_type_id
+      t.timestamps
+    end
+  end
+end
