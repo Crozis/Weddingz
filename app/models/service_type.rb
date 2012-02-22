69630bf09cbac5591d5ca7fec3ea85c202118f16
Nima Izadi <nim.izadi@gmail.com>
2012-02-22 23:59:09 +0100

    Add as_json for service types

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -3,4 +3,13 @@ class ServiceType < ActiveRecord::Base
 
   has_many :service_types_weddings
   has_many :weddings, :through => :service_types_weddings
+
+  def as_json(option) 
+      {
+      id:        self.id,
+      name:      self.name,
+      image_url: self.image_url || '',
+      icon_url:  self.icon_url || ''
+    }
+  end
 end
